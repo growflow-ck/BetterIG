0f08297c46064844485d625fec2520378fb4bd0f
growflow-ck <ckm3dia@gmail.com>
2026-08-27 14:10:20 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-/* Public browser configuration. Never place a Supabase secret/service-role key here. */
-window.BETTERIG_CONFIG = Object.freeze({
-  supabaseUrl: "https://duimvrrtqezpksibbuzo.supabase.co",
-  supabasePublishableKey: "sb_publishable_sA8KyerHFivUHvlv_njOEA_o-CyIfo5",
-  productionUrl: "https://growflow-ck.github.io/BetterIG/"
-});
